c28c334ecd7974235dbc12aaeab3b49154e2b64d
Kevin C Malin <racmalin@yahoo.com>
2020-04-27 03:44:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,15 @@
 def my_each(array)
   i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+end
+
+
+
+def my_each(array)
+  i = 0
   collection = []
   while i < array.length
   collection << yield(array[i])
